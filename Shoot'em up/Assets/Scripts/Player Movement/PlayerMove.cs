3049923e978b91f7b2357ab4258943718087e4bf
mmehrdadd <78869968+mmehrdadd@users.noreply.github.com>
2021-02-13 00:48:46 +0330

    Assets and basic Game core
    assets added and basic game logic has been assigned

---
+++ Shoot'em up/Assets/Scripts/Player Movement/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float Speed = 5f;
+    [SerializeField] float Max_y, Min_y;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y += Time.deltaTime * Speed;
+            if (temp.y > Max_y)
+            {
+                temp.y = Max_y;
+            }
+            transform.position = temp;
+            
+        }
+
+        else if (Input.GetAxis("Vertical") < 0f)
+        {
+            Vector3 temp = transform.position;
+            temp.y -= Time.deltaTime * Speed;
+            if (temp.y < Min_y)
+            {
+                temp.y = Min_y;
+            }
+            transform.position = temp;
+            
+        }
+    }
+}
